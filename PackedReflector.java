08c47e7b75fb0e51bd20828918a710bdeda0b204
Levon Yedigaryan <128414715+LevonYedigaryan@users.noreply.github.com>
2023-05-06 01:47:23 +0400

    Add files via upload

---
+++ PackedReflector.java
@@ -0,0 +1,20 @@
+public class PackedReflector extends PackedRotor{
+	public PackedReflector(String format) throws IncorrectFormatException, RotorBoundsException{
+		super(format);
+		String sorted=getPattern().getSorted();
+		String cypher=getPattern().getCypher();
+		for(int i=0;i<=getMaximalIndex();i++){
+			if(i!=getPattern().sortedIndexOf(cypher.charAt(getPattern().sortedIndexOf(cypher.charAt(i))))){
+				throw new IncorrectFormatException("Incorrect format: The format does not correspond to the one of a reflector.");
+			}
+		}
+	}
+
+	public PackedReflector(PackedReflector reflector) throws IncorrectFormatException, RotorBoundsException{
+		this(reflector.getPattern());
+	}
+
+	public PackedReflector(Pattern pattern) throws IncorrectFormatException, RotorBoundsException{
+		this(pattern.toString());
+	}
+}
